57e4ec33a7c1c53d579a93f5a5d1308db3088130
Gigan-AI <Giganeric974@gmail.com>
2026-09-14 16:18:58 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
 const express = require("express");
 const cors = require("cors");
-const Database = require("better-sqlite3");
+const { Pool } = require("pg");
 const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
 
